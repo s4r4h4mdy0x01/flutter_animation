9708f915a4e4fff8e6931ef179638cb20d2ead37
s4r4h4mdy0x01 <sara101hamdy@gmail.com>
2024-08-30 04:37:27 +0300

    add splash screen

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animation/core/router/routes.dart';
 import 'package:flutter_animation/home.dart';
 import 'package:lottie/lottie.dart';
 
@@ -14,15 +15,56 @@ class _SplashScreenState extends State<SplashScreen>
   late AnimationController _animationController;
 
   @override
+  void initState() {
+    super.initState();
+    _initializeAnimationController();
+  }
+
+  void _initializeAnimationController() {
+    _animationController = AnimationController(vsync: this);
+  }
+
+  @override
   void dispose() {
     _animationController.dispose();
     super.dispose();
   }
 
-  @override
-  void initState() {
-    super.initState();
-    _animationController = AnimationController(vsync: this);
+  void _onLottieLoaded(LottieComposition composition) {
+    _animationController
+      ..duration = composition.duration * 0.3
+      ..forward().then((_) {
+        _navigateToHomeScreen();
+      });
+  }
+
+  void _navigateToHomeScreen() {
+    Navigator.of(context).pushReplacement(
+      PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) =>
+            const HomeScreen(),
+        transitionsBuilder: _buildTransition,
+      ),
+    );
+  }
+
+  Widget _buildTransition(
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    var begin = const Offset(0.0, 1.0);
+    var end = Offset.zero;
+    var curve = Curves.easeIn;
+
+    var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+    var offsetAnimation = animation.drive(tween);
+
+    return SlideTransition(
+      position: offsetAnimation,
+      child: child,
+    );
   }
 
   @override
@@ -33,33 +75,7 @@ class _SplashScreenState extends State<SplashScreen>
           fit: BoxFit.cover,
           'assets/animation/animation_splash.json',
           controller: _animationController,
-          onLoaded: (composition) {
-            _animationController
-              ..duration = composition.duration * 0.3
-              ..forward().then((_) {
-                Navigator.of(context).pushReplacement(
-                  PageRouteBuilder(
-                    pageBuilder: (context, animation, secondaryAnimation) =>
-                        HomeScreen(),
-                    transitionsBuilder:
-                        (context, animation, secondaryAnimation, child) {
-                      var begin = Offset(0.0, 1.0);
-                      var end = Offset.zero;
-                      var curve = Curves.easeIn;
-
-                      var tween = Tween(begin: begin, end: end)
-                          .chain(CurveTween(curve: curve));
-                      var offsetAnimation = animation.drive(tween);
-
-                      return SlideTransition(
-                        position: offsetAnimation,
-                        child: child,
-                      );
-                    },
-                  ),
-                );
-              });
-          },
+          onLoaded: _onLottieLoaded,
         ),
       ),
     );
